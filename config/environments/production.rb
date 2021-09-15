33e9538e2c0501d47b8173cd421c260371fdcc65
Ray Bradley <ray.bradley@gmail.com>
2021-09-15 12:17:14 -0400

    Filter Events index via querystring. Rough in K8S database migrations

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors  = true
   config.action_mailer.delivery_method        = :smtp
   config.action_mailer.perform_caching        = false
-  config.action_mailer.default_url_options    = { host: 'scoutplan.org', protocol: :https }
+  config.action_mailer.default_url_options    = { host: 'go.scoutplan.org', protocol: :https }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
