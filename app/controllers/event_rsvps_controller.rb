5b1c7a2ad286a69a32cdd51d4cb6fbde26ba9a98
Ray Bradley <ray.bradley@gmail.com>
2021-09-20 06:57:47 -0400

    Implemented going/not going on Events#organize page

--- app/controllers/event_rsvps_controller.rb
+++ app/controllers/event_rsvps_controller.rb
@@ -3,18 +3,19 @@
 # only intended to be called via XHR...no view exist
 # for this controller
 class EventRsvpsController < ApplicationController
-  def update
-    @rsvp = EventRsvp.find(params[:id])
-    @rsvp.update!(event_rsvp_params)
+  def create
+    @event = Event.find(params[:event_id])
 
-    respond_to do |format|
-      format.js
-    end
-  end
+    # these two lines are cribbed from Events#organize...DRY it out
+    @non_respondents = @event.rsvp_tokens.collect(&:user) - @event.rsvps.collect(&:user)
+    @non_invitees = @event.unit.members - @event.rsvp_tokens.collect(&:user) - @event.rsvps.collect(&:user)
 
-  private
+    @user = User.find(params[:user_id])
+    @response = params[:response]
+    @rsvp = EventRsvp.find_or_create_by(event: @event, user: @user)
+    @rsvp.response = @response
+    @rsvp.save!
 
-  def event_rsvp_params
-    params.require(:event_rsvp).permit(:response)
+    respond_to :html, :json, :js
   end
 end
