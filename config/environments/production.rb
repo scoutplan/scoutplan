9ccfd455f18ca599cbb3566b1b49e4647353a46f
Ray Bradley <ray.bradley@gmail.com>
2021-10-16 09:07:39 -0400

    Logging to Papertrail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,7 @@
 require 'active_support/core_ext/integer/time'
 
 Rails.application.configure do
+  config.logger = RemoteSyslogLogger.new('logs6.papertrailapp.com', 46920)
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
