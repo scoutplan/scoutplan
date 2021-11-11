5f8d8dfb545a161c5ff21a3fbab9f0281a518b80
Ray Bradley <ray.bradley@gmail.com>
2021-11-11 07:53:53 -0500

    Activity mdal roughed in

--- app/controllers/event_activities_controller.rb
+++ app/controllers/event_activities_controller.rb
@@ -1,5 +1,26 @@
+# frozen_string_literal: true
+
+# handles Activities, which are children of Events
 class EventActivitiesController < ApplicationController
+  before_action :find_event, only: [:create, :new]
+
+  def new
+    @activity = @event.activities.new
+    respond_to :js
+  end
+  
   def update
     ap params
+  end
+
+  def create
+    @activity = @event.activities.new(activity_params)
+    @activity.save!
+  end
+
+  private
+
+  def find_event
+    @event = Event.find(params[:event_id])
   end
 end
