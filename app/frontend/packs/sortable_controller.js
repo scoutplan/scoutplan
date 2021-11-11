6f0da4354e46fb70a2d6d882e230bddbcbdf5f50
Ray Bradley <ray.bradley@gmail.com>
2021-11-10 22:41:02 -0500

    Got cross-list dragging working

--- app/frontend/packs/sortable_controller.js
+++ app/frontend/packs/sortable_controller.js
@@ -1,9 +1,21 @@
 import Sortable from "stimulus-sortable"
+import { patch } from '@rails/request.js'
 
 export default class extends Sortable {
   get options() {
     var result = super.options;
     result['group'] = this.element.dataset.sortableGroupValue;
     return result;
+  }
+
+  end({ item, newIndex, to }) {
+    if (!item.dataset.sortableUpdateUrl) return;
+
+    const data = new FormData();
+    data.append('position', newIndex);
+    data.append('event_id', to.dataset.sortableToId);
+
+    patch(item.dataset.sortableUpdateUrl, { body: data });
+    // fetch(item.dataset.sortableUpdateUrl, { method: 'PATCH', body: data })
   }
 }
