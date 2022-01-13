3faa4c9d4d8ab1dc41e0f3aa4b2634fd4771b393
Ray Bradley <ray.bradley@gmail.com>
2022-01-13 09:36:51 -0500

    Light MemberNotifier refactor

--- app/models/member_notifier.rb
+++ app/models/member_notifier.rb
@@ -7,6 +7,8 @@
 # preferences and policies.
 
 # A Notifier class for sending messages to existing members (e.g. event reminders, etc.)
+# the send_email and send_text methods allow us to pass in blocks, and then are responsible
+# for honoring business logic and member preferences in a centralized way
 class MemberNotifier
   def initialize(member)
     @member = member
@@ -16,21 +18,21 @@ class MemberNotifier
     return unless @member.contactable?
 
     send_email { |recipient| MemberMailer.with(member: recipient).test_email.deliver_later }
-    send_text(proc { |recipient| TestTexter.new(recipient).send_message })
+    send_text  { |recipient| TestTexter.new(recipient).send_message }
   end
 
   def send_digest
     return unless Flipper.enabled?(:digest, @member)
 
     send_email { |recipient| MemberMailer.with(member: recipient).digest_email.deliver_later }
-    send_text(proc { |recipient| DigestTexter.new(recipient).send_message })
+    send_text  { |recipient| DigestTexter.new(recipient).send_message }
   end
 
   def send_daily_reminder
     return unless daily_reminder?
 
     send_email { |recipient| MemberMailer.with(member: recipient).daily_reminder_email.deliver_later }
-    send_text(proc { |recipient| DailyReminderTexter.new(recipient).send_message })
+    send_text  { |recipient| DailyReminderTexter.new(recipient).send_message }
   end
 
   private
@@ -49,7 +51,7 @@ class MemberNotifier
   end
 
   # pipe all SMS through here to enforce common business rules
-  def send_text(block)
+  def send_text(&block)
     return unless @member.contactable?
     return unless @member.settings(:communication).via_sms
 
