81b3986d71df3e96718b6f1904e3857b9051bc1f
Ray Bradley <ray.bradley@gmail.com>
2022-02-03 09:21:40 -0500

    Fix reminder emails being blank

--- spec/mailers/previews/member_mailer_preview.rb
+++ spec/mailers/previews/member_mailer_preview.rb
@@ -10,8 +10,15 @@ class MemberMailerPreview < ActionMailer::Preview
 
   def daily_reminder_email
     member = Unit.first.members.first
-    member.unit.events.where(title: 'Preview Test Event').destroy_all
-    event = member.unit.events.new(title: 'Preview Test Event', starts_at: 3.hours.from_now, ends_at: 4.hours.from_now, status: :published, event_category: member.unit.event_categories.first)
+    member.unit.events.where(title: "Preview Test Event").destroy_all
+    event = member.unit.events.new(
+      title: "Preview Test Event",
+      starts_at: 3.hours.from_now,
+      ends_at: 4.hours.from_now,
+      status: :published,
+      location: "Meeting place",
+      event_category: member.unit.event_categories.first
+    )
     event.save!
     MemberMailer.with(member: member).daily_reminder_email
   end
