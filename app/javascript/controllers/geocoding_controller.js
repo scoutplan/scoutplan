b64d117704a22b24cec41cbe1c64b413ebcd97e9
Ray Bradley <ray.bradley@gmail.com>
2022-02-06 21:54:40 -0500

    Got Mapbox SDK client initialized in Stimulus controller

--- app/javascript/controllers/geocoding_controller.js
+++ app/javascript/controllers/geocoding_controller.js
@@ -1,43 +1,43 @@
-// import { Controller } from "@hotwired/stimulus"
-// import { MapboxClient } from "@mapbox/mapbox-sdk"
+import { Controller } from "@hotwired/stimulus"
 
-// export default class extends Controller {
-//   static targets = [ "query" ];
+export default class extends Controller {
+  static targets = [ "query" ];
+  static values = {
+    mapboxToken: String
+  }
 
-//   initialize() {
-//     this.geocodingService = mbxGeocoding({ accessToken: this.mapboxToken });
-//   }
+  initialize() {
+    console.log(this.mapboxTokenValue);
+    this.mapboxClient = mapboxSdk({ accessToken: this.mapboxTokenValue });
+  }
+  
+  connect() {
+    this.createQueryResultElement();
+  }
 
-//   connect() {
-//     console.log("Geocoding connected!!!");
-//     this.createQueryResultElement();
-//   }
+  performQuery(event) {
+    console.log(event);
+    var searchTerm = this.queryTarget.value;
+    var config = { query: searchTerm };
 
-//   performQuery(event) {
-//     console.log(event);
-//     var searchTerm = this.queryTarget.value;
-//     var config = { query: searchTerm };
+    // attempt to get user location through the browser
+    navigator.geolocation.getCurrentPosition(function(position) {
+      config.proximity = [
+        position.coords.longitude,
+        position.coords.latitude,
+      ];
+    });
 
-//     // attempt to get user location through the browser
-//     navigator.geolocation.getCurrentPosition(function(position) {
-//       config.proximity = [
-//         position.coords.longitude,
-//         position.coords.latitude,
-//       ];
-//     });
+    this.mapboxClient.geocoding.forwardGeocode(config)
+      .send()
+      .then(function(response) {
+        console.log(response.body);
+      });
+  }
 
-//     console.log(config);
-
-//     this.geocodingService.forwardGeocode(config)
-//       .send()
-//       .then(function(response) {
-//         console.log(response.body);
-//       });
-//   }
-
-//   createQueryResultElement() {
-//     console.log("Geocoding connected!!!");
-//     // TODO: create a div element
-//     // TODO: use popper to position it
-//   }
-// }
+  createQueryResultElement() {
+    console.log("Geocoding connected!!!");
+    // TODO: create a div element
+    // TODO: use popper to position it
+  }
+}
