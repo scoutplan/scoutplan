36055ad8d237a97fcaa2b49ce929a13f19163303
Ray Bradley <ray.bradley@gmail.com>
2022-03-30 08:50:19 -0400

    Rough in Unit home page, thus far containing a 'My RSVPs' view

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
 
+# controller for handling base URL naviations. For now it just
+# bounces the user to the most recent or first Unit they're a member of
 class HomeController < ApplicationController
+  # GET /
   def index
     current_unit_id = cookies[:current_unit_id]
-    redirect_to unit_events_path(Unit.find(current_unit_id) || current_user.unit_memberships.first.unit)
-  rescue
-    redirect_to unit_events_path(current_user.unit_memberships.first.unit)
+    redirect_to unit_path(Unit.find(current_unit_id) || current_user.unit_memberships.first.unit)
   end
 end
