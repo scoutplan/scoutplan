6f0e803fe2bb775ba17b5d8ddc2d541f07e6bec6
Ray Bradley <ray.bradley@gmail.com>
2022-04-21 08:55:45 -0400

    Add TTL to MagicLinks as prep for refactor

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_17_102906) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_21_124005) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -227,6 +227,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_17_102906) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "path", null: false
+    t.integer "time_to_live", default: 432000
   end
 
   create_table "member_relationships", force: :cascade do |t|
