df57eb6ccef802dc7ef2ba746ab9e4c7289c2229
Ray Bradley <ray.bradley@gmail.com>
2022-05-02 07:51:01 -0400

    Sundry cosmetic fixes. Devise layout work. Bump Blazer gem

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,11 +1,12 @@
 # frozen_string_literal: true
 
+# base Mailer
 class ApplicationMailer < ActionMailer::Base
-  default from: 'info@scoutplan.org'
-  layout 'mailer'
+  default from: "info@scoutplan.org"
+  layout "mailer"
   before_action :set_time_zone
 
   def set_time_zone
-    Time.zone = 'Eastern Time (US & Canada)'
+    Time.zone = "Eastern Time (US & Canada)"
   end
 end
