6be707153eb9a4e06ea4baaf00d23dc3a3ffb682
Ray Bradley <ray.bradley@gmail.com>
2022-06-14 18:47:45 -0400

    Prevent Organize badge from wrapping on events index. Resolves #396

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,6 +3,8 @@
 # Controller for sending messages. Interfaces between
 # UI and *Notifier classes (e.g. MemberNotifier)
 class MessagesController < UnitContextController
+  before_action :find_message, except: [ :index, :new, :create ]
+
   def index
     @draft_messages = current_member.messages.draft
     @sent_messages = current_member.messages.sent
@@ -11,5 +13,27 @@ class MessagesController < UnitContextController
   def new
     authorize current_member.messages.new
     @message = current_member.messages.new
+  end
+
+  def create
+    @message = @unit.messages.new(message_params)
+    @message.author = current_member
+    @message.status = "draft" if params[:commit] == "Save Draft" # < this is bogus
+    if @message.save!
+      redirect_to unit_messages_path(@unit), notice: "Message created"
+    end
+  end
+
+  def edit
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:title, :body)
+  end
+
+  def find_message
+    @message = Message.find(params[:id])
   end
 end
