bf7f5efd3b50b95289a3a5d63dd5a2e61bef2ab5
Ray Bradley <ray.bradley@gmail.com>
2022-07-22 08:57:12 -0400

    As 'as of' date to PDF filename

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -59,7 +59,7 @@ class EventsController < UnitContextController
   def render_printable_calendar
     render(
       locals: { events_by_month: calendar_events },
-      pdf: "#{@unit.name} Schedule",
+      pdf: "#{@unit.name} Schedule as of #{Date.today.strftime('%-d %B %Y')}",
       layout: "pdf",
       encoding: "utf8",
       orientation: "landscape",
