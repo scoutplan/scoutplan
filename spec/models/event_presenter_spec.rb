6cb0a89a4b6af3d46070829dab3f913829877db4
Ray Bradley <ray.bradley@gmail.com>
2022-09-05 12:48:22 -0400

    Rough in My RSVPs page. Fair amount of refactor in support thereof.

--- spec/models/event_presenter_spec.rb
+++ spec/models/event_presenter_spec.rb
@@ -4,8 +4,7 @@ require "rails_helper"
 
 RSpec.describe Event, type: :model do
   before do
-    @current_member = FactoryBot.create(:member)
-    @presenter = EventPresenter.new(current_member: @current_member)
+    @member = FactoryBot.create(:member)
   end
 
   describe "single_day" do
@@ -15,7 +14,7 @@ RSpec.describe Event, type: :model do
         starts_at: Time.now.beginning_of_day + 1.hour,
         ends_at: Time.now.beginning_of_day + 2.hours
       )
-      presenter = EventPresenter.new(event: event)
+      presenter = EventPresenter.new(event, @member)
       expect(presenter.single_day?).to be_truthy
     end
 
@@ -25,19 +24,15 @@ RSpec.describe Event, type: :model do
         starts_at: Time.now.beginning_of_day + 1.hour,
         ends_at: Time.now.beginning_of_day + 26.hours
       )
-      presenter = EventPresenter.new(event: event)
+      presenter = EventPresenter.new(event, @member)
       expect(presenter.single_day?).to be_falsey
     end
   end
 
   describe "family_context_name" do
     it "appends (you) to current user" do
-      expect(@presenter.family_context_name(@current_member)).to eq("#{@current_member.display_first_name} (you)")
+      presenter = EventPresenter.new(nil, @member)
+      expect(presenter.family_context_name(@member)).to eq("#{@member.display_first_name} (you)")
     end
-
-    # it 'does not append (you) to everyone else' do
-    #   another_member = FactoryBot.build(:member)
-    #   expect(@presenter.family_context_name(another_member)).to eq("#{@current_member.display_first_name}")
-    # end
   end
 end
