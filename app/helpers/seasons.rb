0843724e96b5ee5b604adc0199eddd2d218a5b60
Ray Bradley <ray.bradley@gmail.com>
2022-09-17 13:14:49 -0400

    Limit printed calendar to this season. Fixes #565.

--- app/helpers/seasons.rb
+++ app/helpers/seasons.rb
@@ -11,4 +11,14 @@ module Seasons
   def next_season_ends_at
     next_season_starts_at.advance(years: 1)
   end
+
+  def this_season_starts_at
+    start_date = Date.today.beginning_of_month
+    start_date = start_date.advance(months: -1) until start_date.month == 8
+    start_date
+  end
+
+  def this_season_ends_at
+    this_season_starts_at.advance(years: 1)
+  end
 end
