170a0f4b7053dc7068e0e2fd6466c34cea38096f
Ray Bradley <ray.bradley@gmail.com>
2022-09-22 06:25:23 -0400

    Create EventLocation model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_07_114825) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_20_185857) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -181,6 +181,14 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_07_114825) do
     t.string "name"
     t.string "glyph"
     t.string "color"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "event_locations", force: :cascade do |t|
+    t.integer "event_id", null: false
+    t.integer "location_id", null: false
+    t.string "key", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
---
+++ spec/factories/event_locations.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :event_location do
+    event_id { 1 }
+    location_id { 1 }
+    key { "MyString" }
+  end
+end
