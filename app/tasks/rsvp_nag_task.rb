95a1a73404ef53559cab32c77fba2196e8e96226
Ray Bradley <ray.bradley@gmail.com>
2022-10-04 20:51:03 -0400

    Usability tweaks. Communication page refinement.

--- app/tasks/rsvp_nag_task.rb
+++ app/tasks/rsvp_nag_task.rb
@@ -5,14 +5,32 @@
 # (b) event is less than 30 days away, and
 # and (c) the member's family RSVP is incomplete
 class RsvpNagTask < UnitTask
+  TASK_KEY = "rsvp_nag"
+
+  def self.add_to_unit(unit)
+    unit.tasks.find_or_create_by(key: TASK_KEY, type: "DailyReminderTask")
+  end
+
+  def description
+    "RSVP nag"
+  end
+
   def perform
     Rails.logger.info { "Performing RsvpNagTask for #{taskable}"}
     unit.members.each do |member|
-      notifier = RsvpNagNotifier.new(member)
-      notifier.perform
-    rescue StandardError => error
-      Rails.logger.error { error.message }
+      perform_for_member(member)
+    rescue StandardError => e
+      Rails.logger.error { e.message }
     end
     super
+  end
+
+  private
+
+  def perform_for_member(member)
+    return unless Flipper.enabled? :rsvp_nag, member
+
+    notifier = RsvpNagNotifier.new(member)
+    notifier.perform
   end
 end
