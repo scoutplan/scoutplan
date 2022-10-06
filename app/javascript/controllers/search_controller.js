f9514db74bdcf4a0807ca513ff78cd52704499d3
Ray Bradley <ray.bradley@gmail.com>
2022-10-06 17:58:46 -0400

    First pass at global search (gated)

---
+++ app/javascript/controllers/search_controller.js
@@ -0,0 +1,50 @@
+import { Controller } from "@hotwired/stimulus"
+
+const SEARCH_TIMEOUT = 500;
+
+export default class extends Controller {
+  static targets = [ "query" ];
+
+  connect() {
+    console.log("Search controller loaded");
+  }
+
+  show() {
+    document.body.classList.add("showing-search");
+    document.querySelector("#search_query_term").focus();
+  }
+
+  keypress() {
+    clearTimeout(this.timeout);
+    this.timeout = setTimeout(function() {
+      // this.performSearch();  // <- for some reason this doesn't work
+      var queryTerm = document.querySelector("#search_query_term").value;
+      console.log("Performing search with " + queryTerm);
+      
+      var queryTermField = document.querySelector("#query_term");
+      var queryForm = document.querySelector("#query_form");
+  
+      queryTermField.value = queryTerm;
+      document.querySelector("#query_submit").click();
+    }, 250);
+  }
+
+  performSearch(ctx) {
+    var queryTerm = this.queryTarget.value;
+    console.log("Performing search with " + queryTerm);
+    
+    var queryTermField = document.querySelector("#query_term");
+    var queryForm = document.querySelector("#query_form");
+
+    queryTermField.value = queryTerm;
+    queryForm.submit();
+  }  
+}
+
+
+// setTimeout((function(num){return function(){
+//   callback(num);
+// }
+// })(i), loadDelay);​
+
+// https://gist.github.com/charbeltabet/7a1208483785a7002c21617247d5faa3
