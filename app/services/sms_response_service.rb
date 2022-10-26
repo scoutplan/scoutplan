24ed504b4acbf50f89e15d919df1179c201cf39a
Ray Bradley <ray.bradley@gmail.com>
2022-10-26 06:28:46 -0400

    Further message form cleanup

--- app/services/sms_response_service.rb
+++ app/services/sms_response_service.rb
@@ -57,12 +57,9 @@ class SmsResponseService < ApplicationService
   end
 
   def process_yes_no_response
-    ap "#{candidate_events.count} candidate events"
-
     if candidate_events.count == 1
-      set_extended_context
-      record_rsvp
-      send_confirmation
+      set_single_event_context
+      RsvpService.new(member, event).record_response(body == "yes" ? :accepted : :declined)
       return
     end
 
@@ -94,10 +91,10 @@ class SmsResponseService < ApplicationService
     ap candidate_events.map(&:full_title)
   end
 
-  def set_extended_context
+  def set_single_event_context
     self.event = candidate_events.first
     self.unit = event.unit
-    self.member = unit.members.find_by(user: self.user)
+    self.member = unit.members.find_by(user: user)
   end
 
   # resolve a User from the phone number
