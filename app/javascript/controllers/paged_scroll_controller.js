8a3e645eb6617d68cd731753a9a79a294451273f
Ray Bradley <rgb@fastmail.com>
2022-11-04 20:15:38 -0400

    Add scroll buttons to planner view

--- app/javascript/controllers/paged_scroll_controller.js
+++ app/javascript/controllers/paged_scroll_controller.js
@@ -3,11 +3,15 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = [ "left", "right", "scrollable" ];
 
-  initialize() {
-
+  scrollLeft(event) {
+    this.scrollableTarget.scrollLeft -= this.scrollableTarget.offsetWidth * 0.8;
+    // event.target.scroll({ left: -this.scrollableTarget.offsetWidth, behavior: 'smooth' });
+    event.preventDefault();
   }
-  
-  connect() {
-    console.log("Paged scroll connected!");
+
+  scrollRight(event) {
+    this.scrollableTarget.scrollLeft += this.scrollableTarget.offsetWidth * 0.8;
+    // event.target.scroll({ left: this.scrollableTarget.offsetWidth, behavior: 'smooth' });
+    event.preventDefault();
   }
 }
