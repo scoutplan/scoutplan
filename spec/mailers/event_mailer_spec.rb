d1bf6e9d9e9d0ecfd4bf9c88d1c4ca899c9c33f9
Ray Bradley <rgb@fastmail.com>
2022-11-08 15:59:16 -0500

    SMS event prompt progress

--- spec/mailers/event_mailer_spec.rb
+++ spec/mailers/event_mailer_spec.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe EventMailer, type: :mailer do
   # describe 'token_invitation_email' do
--- spec/requests/documents_spec.rb
+++ spec/requests/documents_spec.rb
@@ -1,4 +1,4 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe "Documents", type: :request do
   describe "GET /index" do
