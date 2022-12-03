cb6128881890e8f3f664e920708dc25ff2d68124
Ray Bradley <rgb@fastmail.com>
2022-12-03 09:32:05 -0500

    Initial commit

---
+++ app/tasks/event_organizer_digest_task.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+# Recurring task to send daily reminders to members
+class DailyReminderTask < UnitTask
+  def description
+    "Daily digest for event organizers"
+  end
+
+  def perform
+    Time.zone = unit.settings(:locale).time_zone
+    find_events
+    send_digests if @events.count.positive?
+    super
+  end
+
+  private
+
+  def find_events
+    unit.events.future.select { |e| e.organizers.present? }
+  end
+
+  def send_digest(event, organizer)
+    new_rsvps = event.rsvps.where("created_at > ?", last_ran_at || event.created_at).group_by(&:response)
+    OrganizerMailer.with(event: event, organizer: organizer, new_rsvps: new_rsvps)
+                   .daily_digest_email.send_later
+  end
+
+  def send_digests
+    @events.each do |event|
+      event.organizers.each do |organizer|
+        send_digest(event, organizer)
+      end
+    end
+  end
+end
