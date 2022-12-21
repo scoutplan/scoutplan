a9250dfae5cfd289860284c73d8ea4258f73469c
Ray Bradley <rgb@fastmail.com>
2022-12-21 08:33:07 -0500

    Event shifts display on RSVP page

--- app/models/event_shift.rb
+++ app/models/event_shift.rb
@@ -1,4 +1,13 @@
 class EventShift < ApplicationRecord
   belongs_to :event
   validates_presence_of :name
+
+  def accepted_rsvps
+    rsvps = event.rsvps.accepted
+    rsvps.select { |rsvp| rsvp.event_shift_ids.include?(id) }
+  end
+
+  def attendees
+    accepted_rsvps.map(&:member)
+  end
 end
