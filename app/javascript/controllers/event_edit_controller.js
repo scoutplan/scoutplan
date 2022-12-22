c5db5bbd0fee93bc0b772858dba1e2f170be8010
Ray Bradley <rgb@fastmail.com>
2022-12-22 18:52:05 -0500

    Roughed in document_library

--- app/javascript/controllers/event_edit_controller.js
+++ app/javascript/controllers/event_edit_controller.js
@@ -3,6 +3,11 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = [ "deleteform" ];
 
+  browseDocumentLibrary(event) {
+    document.querySelector("#document_library_overlay").classList.remove("hidden");
+    event.preventDefault();
+  }
+
   delete(event) {
     this.deleteformTarget.submit();
     console.log("delete this!");
@@ -12,6 +17,11 @@ export default class extends Controller {
   showLocation() {
     document.querySelector("#new_location_overlay").classList.remove("hidden");
     document.querySelector("#location_name").focus();
+  }
+
+  hideDocumentLibrary(event) {
+    document.querySelector("#document_library_overlay").classList.add("hidden");
+    event.preventDefault();
   }
 
   hideLocation(event) {
