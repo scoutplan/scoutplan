9197e6d8bb6849f75159383bba019dbd07cba56e
Ray Bradley <rgb@fastmail.com>
2023-05-22 18:06:51 -0400

    Fix event history view

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_19_163135) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_22_120105) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -530,9 +530,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_19_163135) do
     t.bigint "item_id", null: false
     t.string "event", null: false
     t.string "whodunnit"
-    t.text "object"
     t.datetime "created_at"
-    t.text "object_changes"
+    t.jsonb "object"
+    t.jsonb "object_changes"
     t.index ["item_type", "item_id"], name: "index_versions_on_item_type_and_item_id"
   end
 
