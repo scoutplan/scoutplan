3187c4aa551d4cfee912b28f42968539b49100a8
Ray Bradley <rgb@fastmail.com>
2023-06-19 08:26:18 -0400

    Skip inactive members sending digest

--- spec/models/unit_digest_task_spec.rb
+++ spec/models/unit_digest_task_spec.rb
@@ -19,4 +19,13 @@ RSpec.describe UnitDigestTask, type: :model do
   it "returns the correct description" do
     expect(@task.description).to eq(I18n.t("tasks.unit_digest_description"))
   end
+
+  it "excludes inactive members" do
+    FactoryBot.create(:member, unit: @unit, status: :inactive)
+    FactoryBot.create(:member, unit: @unit, status: :active)
+    @unit.members.each do |member|
+      Flipper.enable(:digest, member)
+    end
+    expect { @task.perform }.to change { ActionMailer::Base.deliveries.count }.by(1)
+  end
 end
