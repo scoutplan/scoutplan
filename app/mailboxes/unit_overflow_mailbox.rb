0bfffe4ea167a4b6cb5cc8fe63f42609ae68fe0c
Ray Bradley <rgb@fastmail.com>
2023-07-04 17:54:48 -0400

    Even more logging

--- app/mailboxes/unit_overflow_mailbox.rb
+++ app/mailboxes/unit_overflow_mailbox.rb
@@ -6,7 +6,10 @@
 #
 class UnitOverflowMailbox < ApplicationMailbox
   def process
+    Rails.logger.warn "Processing overflow mail for unit #{@unit}"
+    admins = @unit.members.admin.active?
+    Rails.logger.warn "Found #{admins.count} admins"
     notification = OverflowMailNotification.with(inbound_email: inbound_email, unit: @unit)
-    notification.deliver_later(@unit.members.admin)
+    notification.deliver_later(admins)
   end
 end
