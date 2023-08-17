fe40b7e925db1bf9ccaf879c2b4eec94f8300b9e
Ray Bradley <rgb@fastmail.com>
2023-08-17 14:56:28 -0400

    Use Redis for caching

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,80 +7,42 @@ Rails.application.default_url_options = {
 
 Rails.application.configure do
   config.logger = RemoteSyslogLogger.new(ENV["LOGGER_HOST"], ENV["LOGGER_PORT"])
-  # config.logger = Logtail::Logger.create_default_logger(ENV.fetch("LOGTAIL_TOKEN"))
 
   config.hosts << "go.scoutplan.org"
   config.hosts << "kit.fontawesome.com"
   config.hosts << ENV["RAILS_PRODUCTION_HOSTS"]
   config.hosts << /10\.\d+\.\d+\.\d+/ # internal IP addresses...leave this here
 
-  # Settings specified here will take precedence over those in config/application.rb.
 
-  # Code is not reloaded between requests.
   config.cache_classes = true
 
-  # Eager load code on boot. This eager loads most of Rails and
-  # your application in memory, allowing both threaded web servers
-  # and those relying on copy on write to perform better.
-  # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
 
-  # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
 
   config.action_controller.perform_caching = true
 
-  # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
-  # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
   config.require_master_key = true
 
-  # Disable serving static files from the `/public` folder by default since
-  # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
 
-  # Compress CSS using a preprocessor.
   config.assets.css_compressor = nil
 
-  # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
 
-  # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  # config.asset_host = "http://assets.example.com"
-
-  # Specifies the header that your server uses for sending files.
-  # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for Apache
-  # config.action_dispatch.x_sendfile_header = "X-Accel-Redirect" # for NGINX
-
-  # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :digitalocean
 
-  # Mount Action Cable outside main process or domain.
-  # config.action_cable.mount_path = nil
-  # config.action_cable.url = "wss://example.com/cable"
-  # config.action_cable.allowed_request_origins = [ "http://example.com", /http:\/\/example.*/ ]
-
-  # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   config.force_ssl = true
+  
   config.ssl_options = { redirect: false } # disable redirect since the LB handles it
 
-  # Include generic and useful information about system operation, but avoid logging too much
-  # information to avoid inadvertent exposure of personally identifiable information (PII).
   config.log_level = ENV.fetch("RAILS_LOG_LEVEL") { "info" }
 
-  # Prepend all log lines with the following tags.
   config.log_tags = [:request_id]
 
-  # Use a different cache store in production.
-  # config.cache_store = :mem_cache_store
+  config.cache_store = :redis_cache_store, { url: ENV["REDIS_URL"] }
 
-  # Use a real queuing backend for Active Job (and separate queues per environment).
-  # config.active_job.queue_name_prefix = "scoutplan_production"
-
-  # RGB 12/31/21: temporarily subbing in the inline adapter. I know, it's not production-
-  # worthy. But Sidekiq is busted (see https://github.com/hotwired/turbo-rails/issues/285#issuecomment-1001690504).
-  # When and as that issue resolves, we'll go back to Sidekiq.
   config.active_job.queue_adapter = :sidekiq
-  # config.active_job.queue_adapter = :inline
 
   config.action_mailbox.ingress = :mailgun 
 
@@ -91,9 +53,6 @@ Rails.application.configure do
     host: ENV["SCOUTPLAN_HOST"],
     protocol: ENV["SCOUTPLAN_PROTOCOL"]
   }
-
-  # RGB 10 Feb 2022: moved these settings from end of file, as recommended at
-  # https://github.com/rails/rails/issues/44161
   config.action_mailer.smtp_settings = {
     user_name: ENV["SMTP_USERNAME"],
     password: ENV["SMTP_PASSWORD"],
@@ -104,29 +63,15 @@ Rails.application.configure do
     enable_starttls_auto: true
   }
 
-  # Ignore bad email addresses and do not raise email delivery errors.
-  # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
-
-  # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
-  # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
 
-  # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
 
-  # Log disallowed deprecations.
   config.active_support.disallowed_deprecation = :log
 
-  # Tell Active Support which deprecation messages to disallow.
   config.active_support.disallowed_deprecation_warnings = []
 
-  # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
-
-  # Use a different logger for distributed setups.
-  # require "syslog/logger"
-  # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new "app-name")
 
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new($stdout)
@@ -134,39 +79,5 @@ Rails.application.configure do
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 
-  # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-
-  # Inserts middleware to perform automatic connection switching.
-  # The `database_selector` hash is used to pass options to the DatabaseSelector
-  # middleware. The `delay` is used to determine how long to wait after a write
-  # to send a subsequent read to the primary.
-  #
-  # The `database_resolver` class is used by the middleware to determine which
-  # database is appropriate to use based on the time delay.
-  #
-  # The `database_resolver_context` class is used by the middleware to set
-  # timestamps for the last write to the primary. The resolver uses the context
-  # class timestamps to determine how long to wait before reading from the
-  # replica.
-  #
-  # By default Rails will store a last write timestamp in the session. The
-  # DatabaseSelector middleware is designed as such you can define your own
-  # strategy for connection switching and pass that into the middleware through
-  # these configuration options.
-  # config.active_record.database_selector = { delay: 2.seconds }
-  # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
-  # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 end
-
-# moving SMTP configuration into mainline config, above, per https://github.com/rails/rails/issues/44161
-
-# ActionMailer::Base.smtp_settings = {
-#   user_name: ENV["SMTP_USERNAME"],
-#   password: ENV["SMTP_PASSWORD"],
-#   domain: ENV["SMTP_DOMAIN"],
-#   address: ENV["SMTP_ADDRESS"],
-#   port: ENV["SMTP_PORT"],
-#   authentication: :plain,
-#   enable_starttls_auto: true
-# }
