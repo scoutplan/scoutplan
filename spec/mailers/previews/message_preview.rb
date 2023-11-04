fba7e9aa555db85e9170837e428f4790354a32a5
Ray Bradley <rgb@fastmail.com>
2023-11-04 18:11:53 -0400

    Restore MessageNotification
    - Cutting back over from MemberNotifier

--- spec/mailers/previews/message_preview.rb
+++ spec/mailers/previews/message_preview.rb
@@ -1,14 +1,11 @@
-# frozen_string_literal: true
-
 require "faker"
 
 class MessagePreview < ActionMailer::Preview
   def message_notification
-    url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
     recipient = Unit.first.members.first
     message = recipient.messages.first
+    message.title = "I can't believe it's not butter!"
     message.body = Faker::Lorem.paragraphs(number: 3).join("\n\n")
-    message.body.concat("\n\n<a href='#{url}'>#{url}</a>")
     MessageMailer.with(recipient: recipient, message: message).message_notification
   end
 end
