cb24f3ebfa27499bc5f373a6a4c6b8c780db6f58
Ray Bradley <rgb@fastmail.com>
2023-12-07 18:34:35 -0500

    Deprecate UnitDigestTask
    We're systematically moving from the proprietary and redundant UnitTask
    mechanism. UnitDigestTask functionality has been moved into SendWeeklyDigestJob which
    leverages Active Job.
    
    - Reconfigured UI to decouple from the Tasks facility
    - Added UI for selecting day and time
    - the Job had already existed for some time
    - Closes #1645

--- app/jobs/send_weekly_digest_job.rb
+++ app/jobs/send_weekly_digest_job.rb
@@ -18,7 +18,9 @@ class SendWeeklyDigestJob < ApplicationJob
 
   def self.next_run_time(unit)
     now = Time.now.in_time_zone(unit.time_zone)
-    result = now.next_occurring(unit.settings(:communication).digest_day_of_week.to_sym)
-    result.change(hour: unit.settings(:communication).digest_hour_of_day.to_i, min: 0, sec: 0).utc
+    day_of_week = unit.settings(:communication).digest_day_of_week.downcase
+    hour_of_day = unit.settings(:communication).digest_hour_of_day.to_i
+    result = now.next_occurring(day_of_week.to_sym)
+    result.change(hour: hour_of_day, min: 0, sec: 0).utc
   end
 end
