d7cbe0a817dd3f6ddad72fb9d672850f58d2d17a
Ray Bradley <rgb@fastmail.com>
2023-12-07 21:36:15 -0500

    Toggle digest configuration fields
    - Hide when digest is disabled

--- app/javascript/switch.js
+++ app/javascript/switch.js
@@ -1,7 +1,7 @@
-document.querySelectorAll("input.switch").forEach(function(elem) {
+document.querySelectorAll(".dp-switch").forEach(function(elem) {
   elem.addEventListener("change", function(event) {
     console.log(event);
-    var wrapper = event.target.closest(".switch-wrapper");
+    var wrapper = event.target.closest(".switch-toggle-container");
     wrapper.classList.toggle("switch-on");
   });
 });
