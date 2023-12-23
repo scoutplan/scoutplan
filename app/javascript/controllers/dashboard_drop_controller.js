73ca9d5aafea126ce4c44c2de4ce543b05782277
Ray Bradley <rgb@fastmail.com>
2023-12-23 15:32:53 -0500

    Drag and drop is successfully posting to endpoints now.

--- app/javascript/controllers/dashboard_drop_controller.js
+++ app/javascript/controllers/dashboard_drop_controller.js
@@ -1,31 +1,42 @@
 import { Controller } from "@hotwired/stimulus"
-
+import { FetchRequest } from "@rails/request.js"
 export default class extends Controller {
   draggedElem = null;
+
+  static values = { unitId: String, eventId: String };
   
   dragover(event) {
-    event.preventDefault();
     const columnElem = event.target.closest(".member-column");
-    if (columnElem == this.draggedElem.closest(".member-column")) {
+    console.log(columnElem);
+
+    if (columnElem == null) {
       event.dataTransfer.dropEffect = "none";
       return;
     }
 
+    // don't drop an object into its own column
+    if (columnElem == this.draggedElem.closest(".member-column")) {
+      event.dataTransfer.dropEffect = "none";
+      return;
+    }
+    
+    // don't drop a unit membership into a unit membership column
+    if (columnElem.id.startsWith("members") && this.draggedElem.id.startsWith("unit_membership")) {
+      event.dataTransfer.dropEffect = "move";
+      return;
+    }
+    
+    event.preventDefault();
     event.dataTransfer.dropEffect = "move";
     columnElem?.classList?.toggle("droptarget", true);  
   }
 
   drag(event) {
     this.draggedElem = event.target;
-    // event.preventDefault();
-    // const columnElem = event.target.closest(".member-column");
-    // columnElem?.classList?.toggle("droptarget", true);    
   }
 
   dragenter(event) {
-    // event.preventDefault();
-    // const columnElem = event.target.closest(".member-column");
-    // columnElem?.classList?.toggle("droptarget", true);
+
   }
 
   dragleave(event) {
@@ -35,49 +46,59 @@ export default class extends Controller {
   }
 
   dragstart(event) {
-    console.log("dragstart" + event.target.id);
     event.dataTransfer.dropEffect = "move";
+    event.dataTransfer.setData("text/plain", event.target.parentElement.id);
   }
 
   drop(event) {
-    console.log(event.target);
+    const columnElem = event.target.closest(".member-column");
+    var response = null;
+    if (!columnElem) { return; }
+    console.log("Dropped " + event.dataTransfer.getData("text/plain") + " onto " + columnElem.id);
+    switch(columnElem.id) {
+      case "rsvps_accepted_column":
+        response = "accepted";
+        break;
+      case "rsvps_declined_column":
+        response = "declined";
+        break;
+      default:
+        response = "delete";
+        break;
+    }
+
+    const idParts = event.dataTransfer.getData("text/plain").split("_");
+    const id = idParts.pop();
+    const objectType = idParts.join("_");
+
+    switch(objectType) {
+      case "unit_membership":
+        this.createRsvp(id, response);
+        break;
+      case "event_rsvp":
+        this.updateRsvp(id, response);
+        break;
+    }
+
+    event.target.classList.toggle("droptarget", false);
   }
 
+  async createRsvp(unitMembershipId, response) {
+    const formData = new FormData();
+    formData.append("event_rsvp[response]", response);
+    formData.append("event_rsvp[unit_membership_id]", unitMembershipId);
+    const request = new FetchRequest("post", this.path, { body: formData, responseKind: "turbo-stream" });
+    await request.perform();
+  }
 
-  // initialize() {}
-  // connect() {}
-  // disconnect() {}
-  // start() {}
-  // stop() {}
-  // pause() {}
-  // resume() {}
-  // destroy() {}
-  // click(event) {}
-  // keydown(event) {}
-  // keyup(event) {}
-  // mousedown(event) {}
-  // mouseup(event) {}
-  // mousemove(event) {}
-  // mouseover(event) {}
-  // mouseout(event) {}
-  // mouseenter(event) {}
-  // mouseleave(event) {}
-  // touchstart(event) {}
-  // touchmove(event) {}
-  // touchend(event) {}
-  // touchcancel(event) {}
-  // selectstart(event) {}
-  // select(event) {}
-  // selectend(event) {}
-  // dragstart(event) {}
-  // drag(event) {}
-  // dragend(event) {}
-  // dragenter(event) {}
-  // dragover(event) {}
-  // dragleave(event) {}
-  // drop(event) {}
-  // focusin(event) {}
-  // focusout(event) {}
-  // focus(event) {}
-  // blur(event) {}
+  async updateRsvp(eventRsvpId, response) {
+    const formData = new FormData();
+    formData.append("event_rsvp[response]", response);
+    const request = new FetchRequest("patch", this.path + `/${eventRsvpId}`, { body: formData, responseKind: "turbo-stream" });
+    await request.perform();
+  }
+
+  get path() {
+    return `/u/${this.unitIdValue}/schedule/${this.eventIdValue}/event_rsvps`;
+  }
 }
