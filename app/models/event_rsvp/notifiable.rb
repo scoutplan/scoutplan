9f48fddaf1127be9685e889c1779f9cf4496c104
Ray Bradley <rgb@fastmail.com>
2024-01-11 14:50:58 -0500

    Replaced EventOrganizerTask
    - New EventRsvpOrganizerNotification
    - Accompanying Job (might be able to drop this is we can get wait_until working in Noticed)

--- app/models/event_rsvp/notifiable.rb
+++ app/models/event_rsvp/notifiable.rb
@@ -4,30 +4,34 @@ module EventRsvp::Notifiable
   extend ActiveSupport::Concern
 
   included do
-    after_save_commit :enqueue_notifications
+    before_save :enqueue_organization_notification, if: :send_organizer_notification?
+    after_save_commit :enqueue_confirmation
   end
 
   private
 
   def confirmation_recipients
-    return unit_membership.family if requires_approval?
+    return unit_membership.family if requires_approval? # <- code smell
 
     [unit_membership]
   end
 
-  def enqueue_notifications
-    EventRsvpConfirmation.with(event_rsvp: self).deliver_later(confirmation_recipients)
-    return unless send_organizer_notification?
+  def enqueue_organization_notification
+    puts "Enqueuing EventRsvpOrganizerNotificationJob for #{event.title}"
+    EventRsvpOrganizerNotificationJob.set(wait_until: send_organizer_notification_at).perform_later(event)
+  end
 
-    EventRsvpOrganizerNotificationJob.set(wait_until: 1.day.from_now).perform_later(event)
+  def enqueue_confirmation
+    EventRsvpConfirmation.with(event_rsvp: self).deliver_later(confirmation_recipients)
   end
 
   def send_organizer_notification_at
     Time.zone = unit.time_zone
-    DateTime.current.next_occurence_of(hour: RUN_TIME).utc
+    Time.current.next_occurrence_of(hour: RUN_TIME).utc
   end
 
   def send_organizer_notification?
-    event.event_rsvps.recent.count == 1
+    puts event.event_rsvps.recent.count.zero?
+    event.event_rsvps.recent.count.zero?
   end
 end
