b040e00405ef08ebb6f5f1c184265d6d2a151934
Ray Bradley <rgb@fastmail.com>
2024-01-23 20:10:36 -0500

    Additional logging for SendMessageJob

--- app/jobs/send_message_job.rb
+++ app/jobs/send_message_job.rb
@@ -2,9 +2,10 @@ class SendMessageJob < ApplicationJob
   queue_as :default
 
   def perform(message, updated_at)
-    Rails.logger.warn "Performing SendMessageJob for message #{message.id}"
+    Rails.logger.warn "SendMessageJob#perform #{message.id}"
     return unless message.updated_at == updated_at
 
+    Rails.logger.warn "Sending #{message.id}"
     message.send!
   end
 end
