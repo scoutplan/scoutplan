eda21ccb8537ce2049d889ef1ff48913345a4d52
Ray Bradley <rgb@fastmail.com>
2024-01-27 16:45:01 -0500

    RSVP run time (#1751)
    RSVP Last Call job was erroneously triggered as a function of the event's start time. It should be a function of the rsvp_closes_at value.
    
    - Failing spec
    - Fix for the above; spec passes
    - Closes #1750

--- spec/concerns/event/remindable_spec.rb
+++ spec/concerns/event/remindable_spec.rb
@@ -2,17 +2,27 @@ require "rails_helper"
 
 RSpec.describe Event::Remindable, type: :concern do
   before do
-    @event = FactoryBot.create(:event, :published, :requires_rsvp)
+    # @event = FactoryBot.create(:event, :published, :requires_rsvp)
+    # @unit = @event.unit
+    @unit = FactoryBot.create(:unit)
+    Time.zone = @unit.time_zone
   end
 
   describe "methods" do
     describe "callbacks" do
       it "enqueues an EventReminderJob" do
-        expect { FactoryBot.create(:event, :published, :requires_rsvp)}.to have_enqueued_job(EventReminderJob)
+        expect { FactoryBot.create(:event, :published, :requires_rsvp) }.to have_enqueued_job(EventReminderJob)
       end
 
       it "enqueues an RsvpLastCallJob" do
-        expect { FactoryBot.create(:event, :published, :requires_rsvp) }.to have_enqueued_job(RsvpLastCallJob)
+        starts_at = 4.days.from_now
+        ends_at = 5.days.from_now
+        rsvp_closes_at = 3.days.from_now.at_end_of_day
+        last_call_lead_time = rsvp_closes_at - Event::Remindable::LAST_CALL_LEAD_TIME
+        rsvp_last_call_at = @unit.in_business_hours(last_call_lead_time)
+
+        expect { FactoryBot.create(:event, :published, :requires_rsvp, unit: @unit, starts_at: starts_at, ends_at: ends_at, rsvp_closes_at: rsvp_closes_at) }
+          .to have_enqueued_job(RsvpLastCallJob).on_queue("default").at(rsvp_last_call_at)
       end
     end
   end
