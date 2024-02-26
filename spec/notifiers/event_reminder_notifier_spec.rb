ed4d812e08b42a4c3be4ec8607d360b145cc32b7
Ray Bradley <rgb@fastmail.com>
2024-02-25 19:12:31 -0500

    Fixed EventReminderNotifier. The :if statements weren't working right. Fixes #1782. Fixed DigestNotifier. It was checking the wrong timestamp. Fixes #1783.

--- spec/notifiers/event_reminder_notifier_spec.rb
+++ spec/notifiers/event_reminder_notifier_spec.rb
@@ -1,6 +1,8 @@
 require "rails_helper"
 
 RSpec.describe EventReminderNotifier do
+  include ActiveJob::TestHelper
+
   before do
     Time.zone = "America/New_York"
     @event = FactoryBot.create(:event, starts_at: Time.current.change(hour: 19, min: 30))
@@ -18,6 +20,8 @@ RSpec.describe EventReminderNotifier do
   it "delivers an email" do
     Flipper.enable(:deliver_email)
     expect { EventReminderNotifier.with(event: @event).deliver([@member]) }.to have_enqueued_job
+    perform_enqueued_jobs
+    perform_enqueued_jobs
   end
 
   it "renders the SMS correctly" do
@@ -25,5 +29,7 @@ RSpec.describe EventReminderNotifier do
     notification = EventReminderNotifier.with(event: @event)
     sms = notification.sms_body(recipient: @member, event: @event, params: {})
     expect(sms).to(include @event.starts_at.in_time_zone(@unit.time_zone).strftime("%A"))
+    perform_enqueued_jobs
+    perform_enqueued_jobs
   end
 end
