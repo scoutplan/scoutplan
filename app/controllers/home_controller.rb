0e12fae8fa45b8dde369691906cc2037338024a4
Ray Bradley <rgb@fastmail.com>
2024-04-19 08:25:25 -0400

    Increase attachment limit (#1845)
    * Initial commit
    
    * Increase attachment size limit
    
    * Hotfix Home Controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,8 @@ class HomeController < ApplicationController
   skip_before_action :authenticate_user!
 
   def index
+    return unless current_user.present?
+
     unit_id = current_user.unit_memberships.first.unit.id
     unit = Unit.find_by(id: unit_id)
     redirect_to unit_events_path(unit_id) and return if unit
