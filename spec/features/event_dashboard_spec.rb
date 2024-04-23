e1bc41f21258a3264efd343a42ebb1ebc4b02282
Ray Bradley <rgb@fastmail.com>
2024-04-23 06:45:24 -0400

    Fixed family RSVP edit bug
    - Failing spec
    - Closes #1848

--- spec/features/event_dashboard_spec.rb
+++ spec/features/event_dashboard_spec.rb
@@ -4,12 +4,13 @@ describe "Event Dashboard", type: :feature do
   before do
     @member = FactoryBot.create(:unit_membership, :admin)
     @unit = @member.unit
+    @admin = FactoryBot.create(:unit_membership, :admin, unit: @unit)
     @unit.update(allow_youth_rsvps: true)
     @event = FactoryBot.create(:event, :requires_rsvp, :published, unit: @unit, allow_youth_rsvps: true)
     @child = FactoryBot.create(:unit_membership, unit: @unit, member_type: "youth", allow_youth_rsvps: true)
     @member.child_relationships.create(child_unit_membership: @child)
     @child_rsvp = @event.event_rsvps.create!(member: @child, response: "accepted_pending", respondent: @child)
-    login_as(@member.user, scope: :user)
+    login_as(@admin.user, scope: :user)
   end
 
   it "sets up correctly" do
@@ -23,9 +24,22 @@ describe "Event Dashboard", type: :feature do
     expect(page).to have_content(@child.display_name)
   end
 
+  describe "RSVPs" do
+    it "allows admin to change RSVP", js: true do
+      path = dashboard_unit_event_path(@unit, @event)
+      visit path
+      click_on @member.display_name
+      modal = find(".modal-dialog")
+      expect(modal).to have_content(@member.first_name)
+      expect(modal).to have_content(@child.first_name)
+      expect(modal).not_to have_content(@admin.first_name)
+    end
+  end
+
   describe "non-admin" do
     before do
       @member.update(role: :member)
+      login_as(@member.user, scope: :user)
     end
 
     it "prevents dashboard access" do
