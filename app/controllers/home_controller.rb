02404dede0404395c2c709fb11bffadb827331aa
Ray Bradley <rgb@fastmail.com>
2024-04-27 06:08:21 -0400

    - Planner layout - Session controller bug - Home controller bug

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
-  skip_before_action :authenticate_user!
+  # skip_before_action :authenticate_user!
 
   def index
     return unless current_user.present?
