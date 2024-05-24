93169a5b9ac8628df4adf0a4d8bdebc1aaa82b0a
Ray Bradley <rgb@fastmail.com>
2024-05-24 07:52:26 -0400

    Handle non-existent unit slugs and non-existent pages

--- app/controllers/web_controller.rb
+++ app/controllers/web_controller.rb
@@ -3,12 +3,18 @@
 class WebController < ActionController::Base
   layout "web"
 
+  # rubocop:disable Metrics/AbcSize
   def index
+    puts "here"
     @unit = Unit.find(params[:unit_id]) if params[:unit_id].present?
     @slug = request.subdomain.split(".").first
     @unit = Unit.find_by(slug: @slug) if @slug.present?
+    head 404 and return unless @unit.present?
 
     @slug = @unit.slug
     @partial_name = "#{@slug}_#{params[:path]}"
+    ap lookup_context.find_all("web/_#{@partial_name}")
+    head 404 and return unless lookup_context.find_all("web/_#{@partial_name}").any?
   end
+  # rubocop:enable Metrics/AbcSize
 end
