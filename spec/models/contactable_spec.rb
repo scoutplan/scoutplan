ceb60db93e5c62b7c12350be6310072e2779b712
Ray Bradley <rgb@fastmail.com>
2024-05-27 10:37:03 -0400

    Fix contactable spec

--- spec/models/contactable_spec.rb
+++ spec/models/contactable_spec.rb
@@ -3,14 +3,15 @@ require "rails_helper"
 RSpec.describe Contactable, type: :model do
   it "is contactable" do
     member = FactoryBot.create(:unit_membership)
-    expect(member.contactable?(via: :sms)).to be_falsey
+    expect(member.contactable?(via: :sms)).to be_truthy
     expect(member.contactable?(via: :email)).to be_truthy
     expect(member.contactable?).to be_truthy
 
-    member.settings(:communication).update!(via_sms: true)
+    member.settings(:communication).update!(via_sms: false)
     member.reload
-    expect(member.contactable?(via: :sms)).to be_truthy
+    expect(member.contactable?(via: :sms)).to be_falsey
 
+    member.settings(:communication).update!(via_sms: true)
     member.settings(:communication).update!(via_email: false)
     member.reload
     expect(member.contactable?(via: :email)).to be_falsey
