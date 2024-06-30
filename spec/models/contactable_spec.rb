0aa112ed539131a16978dccf9a536251f92a09e9
Ray Bradley <rgb@fastmail.com>
2024-06-30 14:39:30 -0400

    Weekly digest updates
    - Fix bug that prevents member communication preferences from being honored
    - New SMS language when there are no upcoming events
    - Fixes #1970

--- spec/models/contactable_spec.rb
+++ spec/models/contactable_spec.rb
@@ -23,4 +23,18 @@ RSpec.describe Contactable, type: :model do
     expect(member.contactable?(via: :email)).to be_falsey
     expect(member.contactable?).to be_falsey
   end
+
+  describe "contact_preference" do
+    it "is false when value is false" do
+      member = FactoryBot.create(:unit_membership)
+      member.settings(:communication).via_sms = false
+      expect(member.contact_preference?(via: :sms)).to be_falsey
+    end
+
+    it "is false when value is 'false'" do
+      member = FactoryBot.create(:unit_membership)
+      member.settings(:communication).via_sms = "false"
+      expect(member.contact_preference?(via: :sms)).to be_falsey
+    end
+  end
 end
--- spec/notifiers/weekly_digest_notifier_spec.rb
+++ spec/notifiers/weekly_digest_notifier_spec.rb
@@ -20,8 +20,22 @@ RSpec.describe WeeklyDigestNotifier do
     expect(WeeklyDigestNotifier.new).to be_a(ScoutplanNotifier)
   end
 
-  it "delivers an email" do
+  describe "Twilio" do
+    it "renders the SMS body" do
+      notifier = WeeklyDigestNotifier.with(unit: @unit)
+      body = notifier.sms_body(recipient: @member, unit: @unit)
+      expect(body).to include("There's nothing on the docket")
+      puts body
 
+      FactoryBot.create(:event, unit: @unit, starts_at: Time.zone.now + 1.day, ends_at: Time.zone.now + 2.days, status: "published")
+      body = notifier.sms_body(recipient: @member, unit: @unit)
+      expect(body).not_to include("There's nothing on the docket")
+      expect(body).to include(@event.title)
+      puts body
+    end
+  end
+
+  it "delivers an email" do
     # skip "This test works standalone, but not in the suite. It's a known issue."
     Flipper.enable(:deliver_email)
 
