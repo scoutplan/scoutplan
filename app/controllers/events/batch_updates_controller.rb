c98e7e6383ca585d48feb77286ca361fbf2ae7a2
Ray Bradley <rgb@fastmail.com>
2024-08-27 22:02:01 -0400

    Change bulk event update logic to properly invalidate caches. Fixes #2000.

--- app/controllers/events/batch_updates_controller.rb
+++ app/controllers/events/batch_updates_controller.rb
@@ -4,8 +4,11 @@ module Events
   class BatchUpdatesController < UnitContextController
     def create
       event_ids = params[:event_ids].split(",")
+      params_hash = event_params.to_h
       @events = current_unit.events.where(id: event_ids)
-      @events.update_all(event_params.to_h)
+
+      @events.find_each { |e| e.update(params_hash) }
+      # @events.update_all(event_params.to_h)
     end
 
     private
