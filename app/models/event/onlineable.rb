4092083e4c585cb4b5c129fcf65cbe545cdc46bd
Ray Bradley <rgb@fastmail.com>
2024-11-11 10:30:28 -0500

    Redesigned event list view to show meeting links in the same place as physical locations. Turned out, evnet display wasn't honoring online event locations and only honoring the old explicit online meeting fields. Hence, the Event and Onlineable models were refactored to handle both. Eventually we'll want to migrate legacy online events to the new event_locations construct.
    - Closes #2070

--- app/models/event/onlineable.rb
+++ app/models/event/onlineable.rb
@@ -6,23 +6,35 @@ module Event::Onlineable
   JOIN_LEAD_TIME = 15.minutes.freeze
 
   included do
-    validate :valid_website_url?
+    validate :valid_online_url?
+  end
+
+  def online?
+    online_url.present?
   end
 
   def joinable?
     starts_at < JOIN_LEAD_TIME.from_now && ends_at.future?
   end
 
-  def hostname
-    URI.parse(website).host
+  def online_url
+    online_location&.url || website
   end
 
-  def valid_website_url?
-    return false if website.blank?
+  def online_location
+    @online_location || event_locations.find_by(location_type: "online")
+  end
 
-    uri = URI.parse(website)
+  def hostname
+    URI.parse(online_url).host
+  end
+
+  def valid_online_url?
+    return false if online_url.blank?
+
+    uri = URI.parse(online_url)
     uri.host.present?
   rescue URI::InvalidURIError
-    errors.add(:website, "Invalid URL")
+    errors.add(:online_url, "Invalid URL")
   end
 end
