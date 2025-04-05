78aa32eed5636532eb53e5c78e1126b63b06cf82
Ray Bradley <rgb@fastmail.com>
2025-04-05 09:37:19 -0400

    Disable dark mode (for now) Massage display of mini dashboard in event list on mobile

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -3,6 +3,7 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 const colors = require('tailwindcss/colors')
 
 module.exports = {
+  darkMode: 'false',
   content: [
     './app/helpers/**/*.rb',
     './app/javascript/**/*.js',
