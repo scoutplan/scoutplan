24ffb3b6d2dbf928ade24302a4acd487da5af9e5
Ray Bradley <rgb@fastmail.com>
2025-04-13 03:16:48 -0400

    Progress

--- app/models/unit/distribution_lists.rb
+++ app/models/unit/distribution_lists.rb
@@ -11,17 +11,21 @@ module Unit::DistributionLists
 
   private
 
-  # rubocop:disable Metrics/AbcSize
   def all_distribution_lists
     description_all = pluralize(members.includes(:user).select(&:contactable?).count, "contactable member")
     description_active = pluralize(members.active.includes(:user).select(&:contactable?).count, "contactable member")
-    description_adults = pluralize(members.active.includes(:user).adult.select(&:contactable?).count, "contactable member")
+    description_adults = pluralize(members.active.includes(:user).adult.select(&:contactable?).count,
+                                   "contactable member")
+
+    # description_all = ""
+    # description_active = ""
+    # description_adults = ""
 
     [
       DistributionList.new(key: "all", name: "Everyone in #{name}", keywords: "everyone", description: description_all),
-      DistributionList.new(key: "active", name: "Active Members", keywords: "everyone", description: description_active),
+      DistributionList.new(key: "active", name: "Active Members", keywords: "everyone",
+                           description: description_active),
       DistributionList.new(key: "adults", name: "Active Adults", keywords: "everyone", description: description_adults)
     ]
   end
-  # rubocop:enable Metrics/AbcSize
 end
