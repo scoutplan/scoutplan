f6e10548a8f16816fdc95ab9137038af32674742
Ray Bradley <rgb@fastmail.com>
2025-07-22 17:57:55 -0400

    Revert "Merge pull request #2163 from scoutplan/message_sending_race_condition"
    This reverts commit a84bcc8cf00636475dfae2494e0cb1caf3b4478a, reversing
    changes made to c62e9f08912da57501a3c6083ef27c82cd33c339.

--- app/models/message/sendable.rb
+++ app/models/message/sendable.rb
@@ -1,5 +1,3 @@
-# This module provides functionality for sending messages, including scheduling and marking them as sent
-# and associated Message lifecycle hook.
 module Message::Sendable
   extend ActiveSupport::Concern
 
@@ -7,11 +5,9 @@ module Message::Sendable
     after_commit :create_send_job!
   end
 
-  # enqueue the Message to be sent
   def create_send_job!
     return unless queued? || outbox?
 
-    Rails.logger.warn "Creating SendMessageJob for message #{id} with status #{status} and #{message_recipients.count} recipients"
     SendMessageJob.set(wait_until: wait_until).perform_later(self, updated_at)
   end
 
@@ -19,8 +15,6 @@ module Message::Sendable
     update(status: :sent)
   end
 
-  # SendMessageJob will call this method to actually send the message. The recipients
-  # method can be found in the Notifiable concern, which is probably a code smell.
   def send!
     Rails.logger.warn "Sending message #{id}"
     MessageNotifier.with(message: self).deliver_later(recipients)
