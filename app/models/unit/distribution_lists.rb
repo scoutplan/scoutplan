b8e731e1935d70ef5e08d2f90271527e2269db17
Ray Bradley <rgb@fastmail.com>
2025-11-07 18:33:01 -0500

    Tag-based distribution lists implemented

--- app/models/unit/distribution_lists.rb
+++ app/models/unit/distribution_lists.rb
@@ -11,21 +11,37 @@ module Unit::DistributionLists
 
   private
 
+  # rubocop:disable Metrics/AbcSize
   def all_distribution_lists
     description_all = pluralize(members.includes(:user).select(&:contactable?).count, "contactable member")
     description_active = pluralize(members.active.includes(:user).select(&:contactable?).count, "contactable member")
     description_adults = pluralize(members.active.includes(:user).adult.select(&:contactable?).count,
                                    "contactable member")
 
-    # description_all = ""
-    # description_active = ""
-    # description_adults = ""
-
-    [
+    result = [
       DistributionList.new(key: "all", name: "Everyone in #{name}", keywords: "everyone", description: description_all),
       DistributionList.new(key: "active", name: "Active Members", keywords: "everyone",
                            description: description_active),
       DistributionList.new(key: "adults", name: "Active Adults", keywords: "everyone", description: description_adults)
     ]
+
+    # unique UnitMembership tags
+    tags = unit.unit_memberships.tag_counts_on(:tags)
+
+    ap "@#{$@}#{$@}#{$@}#$#@$@#{$$}#@"
+    ap tags
+
+    tags.each do |tag|
+      result << DistributionList.new(
+        key:         "tag:#{tag.id}",
+        name:        "Members with tag: #{tag.name}",
+        keywords:    "tag #{tag.name}",
+        description: pluralize(members.tagged_with(tag).includes(:user).select(&:contactable?).count,
+                               "contactable member")
+      )
+    end
+
+    result
   end
+  # rubocop:enable Metrics/AbcSize
 end
