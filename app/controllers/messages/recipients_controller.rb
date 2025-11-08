54a054174bd83b66a65f90ddf76a84f8d2dfbf35
Ray Bradley <rgb@fastmail.com>
2025-11-07 19:23:32 -0500

    Fix tag resolution in messages

--- app/controllers/messages/recipients_controller.rb
+++ app/controllers/messages/recipients_controller.rb
@@ -6,7 +6,12 @@ class Messages::RecipientsController < UnitContextController
   def resolve
     return unless (gid = params[:gid]).present?
 
-    resolveable = GlobalID::Locator.locate(gid)
-    @recipients = MessageRecipient.with_guardians(resolveable.recipients)
+    # we can't use GlobalID to resolve tags, so handle that case separately
+    if gid.start_with?("tag://")
+      @recipients = current_unit.unit_memberships.tagged_with(gid.delete_prefix("tag://")).select(&:contactable?)
+    else
+      resolveable = GlobalID::Locator.locate(gid)
+      @recipients = MessageRecipient.with_guardians(resolveable.recipients)
+    end
   end
 end
