78b346d2e82a7e3c3d3b985a6562ecfcb5e19065
Ray Bradley <rgb@fastmail.com>
2026-08-26 14:23:09 -0400

    Progress

--- app/javascript/controllers/clipboard_controller.js
+++ app/javascript/controllers/clipboard_controller.js
@@ -1,14 +1,35 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = [ "source" ];
+  static targets = [ "source", "feedback" ];
   static values = {
-    text: String 
+    text: String,
+    feedbackText: { type: String, default: "Copied!" },
+    feedbackDuration: { type: Number, default: 2000 }
   };
 
   copy(event) {
     const value = this.textValue || event.target.innerText;
     navigator.clipboard.writeText(value);
     event.preventDefault();
+    this.showFeedback();
+  }
+
+  showFeedback() {
+    if (!this.hasFeedbackTarget) { return; }
+
+    if (this.originalFeedback === undefined) {
+      this.originalFeedback = this.feedbackTarget.innerText;
+    }
+    this.feedbackTarget.innerText = this.feedbackTextValue;
+
+    clearTimeout(this.feedbackTimeout);
+    this.feedbackTimeout = setTimeout(() => {
+      this.feedbackTarget.innerText = this.originalFeedback;
+    }, this.feedbackDurationValue);
+  }
+
+  disconnect() {
+    clearTimeout(this.feedbackTimeout);
   }
 }
